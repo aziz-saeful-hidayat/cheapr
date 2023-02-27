0f69eff813aed0a933314ec99959a9847030407b
Rural Developer <ruraldevusa@gmail.com>
2023-02-27 07:32:20 -0700

    update

--- index.js
+++ index.js
@@ -130,37 +130,11 @@ app.get("/update_trackings/", function (req, res) {
   res.send({ message: "Ok" });
 });
 
-app.get("/bsrcluster/", function (req, res) {
-  let zebra_list = [
-    "https://www.amazon.com/02000BK08345-Zebra-1476ft-Ribbon-1-inch/dp/B00EPMYDUW/",
-    "https://www.amazon.com/02000BK08345-Zebra-1476ft-Ribbon-1-inch/dp/B00EPN2NAI/",
-    "https://www.amazon.com/05095GS06407-Thermal-Transfer-Ribbon-Performance/dp/B001PB2FXI/",
-    "https://www.amazon.com/05095GS06407-Zebra-244ft-Ribbon-0-5-inch/dp/B0141M03FE/",
-    "https://www.amazon.com/05095GS11007-Thermal-Transfer-Ribbon-Performance/dp/B001HOUCS8/",
-    "https://www.amazon.com/10008512-ZEBRA-Z-ULT-2000T-WHITE/dp/B017O2FU6K/",
-    "https://www.amazon.com/10010051-CASE-Z-Select-Direct-Thermal-Labels/dp/B00540NOHM/",
-    "https://www.amazon.com/10010053-2-25in-Direct-Thermal-Z-Select/dp/B00E2TXZZW/",
-    "https://www.amazon.com/10015340-Zebra-Z-Select-4000d-Paper/dp/B017O304JM/",
-    "https://www.amazon.com/102-801-00200-Desktop-Thermal-Transfer-Monochrome/dp/B00HGUZQHY/",
-    "https://www.amazon.com/102-801-00200-Desktop-Thermal-Transfer-Monochrome/dp/B08156GS8C/",
-    "https://www.amazon.com/103-801-00200-Thermal-Transfer-Printer-Monochrome/dp/B09QRM8DC6/",
-    "https://www.amazon.com/10500-2001-0030-THERMAL-BARCODE-PRINTER-NETWORK/dp/B008O67W08/",
-    "https://www.amazon.com/10500-2001-1000-1050020011000-A64E507-203DPI-4-09IN/dp/B000H34Y3O/",
-    "https://www.amazon.com/105SL-printer-direct-thermal-transfer/dp/B00308WCQO/",
-    "https://www.amazon.com/110PAX4-Printhead-203dpi-G57202-1M-Renewed/dp/B0BT8JR8QH/",
-    "https://www.amazon.com/110Xi4-Direct-Thermal-Transfer-Printer/dp/B00FJWE14A/",
-    "https://www.amazon.com/110Xi4-Network-Thermal-Printer-116-801-00201/dp/B09T3QVCYK/",
-    "https://www.amazon.com/110XiIII-Thermal-Printer-Parallel-Interfaces/dp/B0BVBZNG8G/",
-    "https://www.amazon.com/112-701-00200-11270100200-Printer-Monochrome-Thermal/dp/B001APQ2F6/",
-    "https://www.amazon.com/112-741-00000-Barcode-Thermal-Printer-200DPI/dp/B00VVS5DDO/",
-    "https://www.amazon.com/113-801-00200-Tabletop-Printer-Parallel-Monochrome/dp/B07VWKQ8BS/",
-    "https://www.amazon.com/12Pk-5319-Perform-Ribbon-Printers/dp/B0141MLV3M/",
-    "https://www.amazon.com/140Xi4-PRINTER-TABLETOP-PARALLEL-INTERNAL/dp/B00NMXA5EI/",
-    "https://www.amazon.com/170PAX4-Thermal-Label-Printer/dp/B0044ZMXC0/",
-    "https://www.amazon.com/170Xi-III-Thermal-Barcode-170-741-00000-Parallel/dp/B082QNB946/",
-    "https://www.amazon.com/170Xi4-Barcode-Printer-172-851-00200-Renewed/dp/B08SGF3Z5P/",
-  ];
-  bsrcluster("zebra printer");
+app.get("/bsrcluster/:keyword", function (req, res) {
+  if (req.keyword) {
+    console.log(req.keyword);
+    bsrcluster(req.keyword);
+  }
   res.send({ message: "Ok" });
 });
 // Making Express listen on port 3000
